d0e1339031aca2d6698f2ed4b62fcdfa776e1660
Florent Vilmart <florent@2xminteractive.com>
2012-05-10 16:32:27 -0400

    Support for building an iOS framework right away!

---
+++ SocketRocket-simulator/SocketRocket_simulator.h
@@ -0,0 +1,13 @@
+//
+//  SocketRocket_simulator.h
+//  SocketRocket-simulator
+//
+//  Created by Florent on 12-05-10.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SocketRocket_simulator : NSObject
+
+@end
---
+++ SocketRocket-simulator/SocketRocket_simulator.m
@@ -0,0 +1,13 @@
+//
+//  SocketRocket_simulator.m
+//  SocketRocket-simulator
+//
+//  Created by Florent on 12-05-10.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "SocketRocket_simulator.h"
+
+@implementation SocketRocket_simulator
+
+@end
