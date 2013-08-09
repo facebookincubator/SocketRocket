1929cbc8e0b78f7cd5471221793022840e3bbace
adrian <adrian@uken.com>
2013-08-09 15:47:13 -0400

    block on dealloc until all work queue tasks are finished

--- SocketRocket/SRWebSocket.m
+++ SocketRocket/SRWebSocket.m
@@ -381,8 +381,10 @@ static __strong NSData *CRLFCRLF;
     [_inputStream close];
     [_outputStream close];
     
-    sr_dispatch_release(_workQueue);
+    dispatch_queue_t blockQueue = _workQueue;
     _workQueue = NULL;
+    dispatch_sync( blockQueue, ^{} ); // block until the work queue is empty
+    sr_dispatch_release(blockQueue);
     
     if (_receivedHTTPHeaders) {
         CFRelease(_receivedHTTPHeaders);
