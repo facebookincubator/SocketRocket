5a4dc671c5a98e28613d3ddbced373a071658662
Greg Krsak <greg.krsak@gmail.com>
2013-08-18 00:15:37 -0700

    Added failing tests for NSData+SRB64Additions

---
+++ NSDataSRB64AdditionsTests/NSDataSRB64AdditionsTests.h
@@ -0,0 +1,14 @@
+//
+//  NSDataSRB64AdditionsTests.h
+//  NSDataSRB64AdditionsTests
+//
+//  Created by Greg M. Krsak (greg.krsak@gmail.com) on 8/17/13.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NSDataSRB64AdditionsTests : SenTestCase
+
+- (id)nullTerminate:(NSMutableData *)data;
+
+@end
