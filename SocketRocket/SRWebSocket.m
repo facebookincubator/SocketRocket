e4b8259701e12a84fa8bb0dc4a5fcfc455780026
Philippe Casgrain <github@casgrain.com>
2013-10-10 13:53:06 -0400

    Updated for arm64 compatibility
    Note that Xcode requires iOS6 as the earliest deployment platform for cross-
    compilation of arm32 / arm64 code.

--- SocketRocket/SRWebSocket.m
+++ SocketRocket/SRWebSocket.m
@@ -124,7 +124,7 @@ static inline void SRFastLog(NSString *format, ...);
 static NSString *newSHA1String(const char *bytes, size_t length) {
     uint8_t md[CC_SHA1_DIGEST_LENGTH];
     
-    CC_SHA1(bytes, length, md);
+    CC_SHA1(bytes, (CC_LONG)length, md);
     
     size_t buffer_size = ((sizeof(md) * 3 + 2) / 2);
     
@@ -552,7 +552,7 @@ static __strong NSData *CRLFCRLF;
 
 - (void)_initializeStreams;
 {
-    NSInteger port = _url.port.integerValue;
+    UInt32 port = (UInt32)_url.port.unsignedIntegerValue;
     if (port == 0) {
         if (!_secure) {
             port = 80;
@@ -1460,7 +1460,7 @@ static const size_t SRFrameHeaderOverhead = 32;
                 uint8_t buffer[bufferSize];
                 
                 while (_inputStream.hasBytesAvailable) {
-                    int bytes_read = [_inputStream read:buffer maxLength:bufferSize];
+                    NSInteger bytes_read = [_inputStream read:buffer maxLength:bufferSize];
                     
                     if (bytes_read > 0) {
                         [_readBuffer appendBytes:buffer length:bytes_read];
@@ -1670,7 +1670,7 @@ static inline int32_t validate_dispatch_data_partial_string(NSData *data) {
         size = -1;
     }
     
-    return size;
+    return (int32_t)size;
 }
 
 #else
