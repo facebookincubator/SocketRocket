5c99f7dd90b6ee9e00ca536f3754fc6ee07d286a
Jim Schultz <boxenjim@gmail.com>
2014-04-24 10:52:21 -0700

    Fixed base64Encoding deprecation, added the actual HTTP status code with key ‘HTTPResponseStatusCode’ in the NSError userInfo dictionary on status codes of >= 400, gives you the ability to see why a connection fails such as a 401 authentication failure

--- SocketRocket/SRWebSocket.m
+++ SocketRocket/SRWebSocket.m
@@ -438,9 +438,8 @@ static __strong NSData *CRLFCRLF;
     
     if (responseCode >= 400) {
         SRFastLog(@"Request failed with response code %d", responseCode);
-        [self _failWithError:[NSError errorWithDomain:@"org.lolrus.SocketRocket" code:2132 userInfo:[NSDictionary dictionaryWithObject:[NSString stringWithFormat:@"received bad response code from server %ld", (long)responseCode] forKey:NSLocalizedDescriptionKey]]];
+        [self _failWithError:[NSError errorWithDomain:@"org.lolrus.SocketRocket" code:2132 userInfo:@{NSLocalizedDescriptionKey:[NSString stringWithFormat:@"received bad response code from server %ld", (long)responseCode], @"HTTPResponseStatusCode":@(responseCode)}]];
         return;
-
     }
     
     if(![self _checkHandshake:_receivedHTTPHeaders]) {
@@ -501,7 +500,7 @@ static __strong NSData *CRLFCRLF;
         
     NSMutableData *keyBytes = [[NSMutableData alloc] initWithLength:16];
     SecRandomCopyBytes(kSecRandomDefault, keyBytes.length, keyBytes.mutableBytes);
-    _secKey = keyBytes.base64Encoding;
+    _secKey = [keyBytes base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
     assert([_secKey length] == 24);
 
     // Apply cookies if any have been provided
