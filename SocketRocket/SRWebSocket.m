2f0e0f3680d7eb29e13477585f79d462c0ace6bc
bytelee <bytegm@gmail.com>
2014-08-29 12:06:58 +0800

    remove debug info

--- SocketRocket/SRWebSocket.m
+++ SocketRocket/SRWebSocket.m
@@ -572,7 +572,6 @@ static __strong NSData *CRLFCRLF;
         
 #if DEBUG
         [SSLOptions setValue:[NSNumber numberWithBool:NO] forKey:(__bridge id)kCFStreamSSLValidatesCertificateChain];
-        NSLog(@"SocketRocket: In debug mode.  Allowing connection to any root cert");
 #endif
         
         [_outputStream setProperty:SSLOptions
