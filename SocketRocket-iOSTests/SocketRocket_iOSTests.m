ae88a3be38407adb9f0195e6d3d8e6bc6375410f
dodikk <dodikk88.reg@gmail.com>
2015-02-21 20:19:47 +0200

    [carthage] Added iOS framework target and scheme

---
+++ SocketRocket-iOSTests/SocketRocket_iOSTests.m
@@ -0,0 +1,40 @@
+//
+//  SocketRocket_iOSTests.m
+//  SocketRocket-iOSTests
+//
+//  Created by Oleksandr Dodatko on 21/02/2015.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface SocketRocket_iOSTests : XCTestCase
+
+@end
+
+@implementation SocketRocket_iOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
