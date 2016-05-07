6d9bf914256b3b831dac15dadf776aef84d671d7
Nikita Lutsenko <nlutsenko@me.com>
2016-05-06 17:02:03 -0700

    Rewrite, refactor and cleanup autobahn test operations.

--- Tests/Utilities/XCTestCase+SRTAdditions.h
+++ Tests/Utilities/XCTestCase+SRTAdditions.h
@@ -1,22 +1,18 @@
 //
 // Copyright 2012 Square Inc.
 // Portions Copyright (c) 2016-present, Facebook, Inc.
-// 
+//
 // All rights reserved.
-// 
+//
 // This source code is licensed under the BSD-style license found in the
-// LICENSE file in the root directory of this source tree. An additional grant 
+// LICENSE file in the root directory of this source tree. An additional grant
 // of patent rights can be found in the PATENTS file in the same directory.
 //
 
 #import <XCTest/XCTest.h>
 
+@interface XCTestCase (SRTAdditions)
 
-typedef BOOL (^PXPredicateBlock)();
-
-
-@interface XCTest (PXAdditions)
-
-- (void)runCurrentRunLoopUntilTestPasses:(PXPredicateBlock)predicate timeout:(NSTimeInterval)timeout;
+- (void)runCurrentRunLoopUntilTestPasses:(BOOL (^)())predicate timeout:(NSTimeInterval)timeout;
 
 @end
