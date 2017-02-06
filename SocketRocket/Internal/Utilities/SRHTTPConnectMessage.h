ae2ab843903293f92c42df9c196b4a675d23ff5b
Tauseef Mughal <tauseef.mughal@youview.com>
2017-02-06 16:09:48 +0000

    Adding bearer authentication support

--- SocketRocket/Internal/Utilities/SRHTTPConnectMessage.h
+++ SocketRocket/Internal/Utilities/SRHTTPConnectMessage.h
@@ -13,6 +13,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 extern CFHTTPMessageRef SRHTTPConnectMessageCreate(NSURLRequest *request,
                                                    NSString *securityKey,
+                                                   NSString *bearerToken,
                                                    uint8_t webSocketProtocolVersion,
                                                    NSArray<NSHTTPCookie *> *_Nullable cookies,
                                                    NSArray<NSString *> *_Nullable requestedProtocols);
