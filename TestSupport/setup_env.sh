8e0b68a56f43c762b0cbbbf22af3ba8bbe71970e
nnabeyang <nabeyang@gmail.com>
2024-10-12 23:12:15 +0900

    Update setup_env.sh to support Apple Silicon Macs.

--- TestSupport/setup_env.sh
+++ TestSupport/setup_env.sh
@@ -8,23 +8,27 @@
 
 VIRTUALENV_PATH=$1
 
-if [ -d "$VIRTUALENV_PATH" ]; then 
+if [ -d "$VIRTUALENV_PATH" ]; then
 	echo "Virtual Env already installed"
 elif [ -z "$VIRTUALENV_PATH" ]; then
   echo "Usage: ./setup_env.sh <folder path>"
 else
   mkdir $VIRTUALENV_PATH
-
-  pushd $VIRTUALENV_PATH  
-  
-  curl -L -o virtualenv.pyz https://bootstrap.pypa.io/virtualenv.pyz
-  
-  popd
-
-  python $VIRTUALENV_PATH/virtualenv.pyz $VIRTUALENV_PATH
-
+  # Please ensure that `python` command refers to Python 2.7.18 to avoid compatibility issues.
+  # If you are using asdf, make sure to install and set Python 2.7.18 with the following commands:
+  #
+  # asdf install python 2.7.18
+  # asdf local python 2.7.18
+  # pip install virtualenv
+  python -m virtualenv $VIRTUALENV_PATH
   source $VIRTUALENV_PATH/bin/activate
-	pip install autobahntestsuite
-  
+
+  # Make sure openssl@1.1 is installed
+  brew install openssl@1.1
+
+  pip install Twisted==15.5
+  LDFLAGS="-L$(brew --prefix openssl@1.1)/lib" CFLAGS="-I$(brew --prefix openssl@1.1)/include" pip install cryptography
+  pip install autobahntestsuite
+
   echo "Environment succesfully set up in $VIRTUALENV_PATH."
 fi
