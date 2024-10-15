2204e53e9276ecb1a0e468233e68141a1739404d
nnabeyang <nabeyang@gmail.com>
2024-10-15 12:00:47 -0700

    Ensure readyState is set to SR_CLOSED on successful WebSocket closure (#693)
    Summary:
    This PR fixes an issue where the `readyState` was not properly set to `SR_CLOSED` when the WebSocket closed successfully.
    
    ### Changes:
    - Previously, during a successful WebSocket closure, the `readyState` remained at `SR_CLOSING` and was not updated to `SR_CLOSED`.
    - With this fix, the `readyState` is now set to `SR_CLOSED` before the delegate method responsible for handling the WebSocket closure is called.
    
    Pull Request resolved: https://github.com/facebookincubator/SocketRocket/pull/693
    
    Reviewed By: nlutsenko
    
    Differential Revision: D64108852
    
    Pulled By: cipolleschi
    
    fbshipit-source-id: d683d951e3b0069f1e909c091e8bc4e5d5066dab

--- SocketRocket/SRWebSocket.m
+++ SocketRocket/SRWebSocket.m
@@ -1114,6 +1114,7 @@ static const uint8_t SRPayloadLenMask   = 0x7F;
         }
 
         if (!_failed) {
+            self.readyState = SR_CLOSED;
             [self.delegateController performDelegateBlock:^(id<SRWebSocketDelegate>  _Nullable delegate, SRDelegateAvailableMethods availableMethods) {
                 if (availableMethods.didCloseWithCode) {
                     [delegate webSocket:self didCloseWithCode:self->_closeCode reason:self->_closeReason wasClean:YES];
